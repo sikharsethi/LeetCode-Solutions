bc50cdc16351f82d34c44bd2ed4a9c336c03ee1d
Sikhar Sethi <sikharsethi5@gmail.com>
2026-02-19 23:13:43 +0530

    added LongestCommonPrefix solution

---
+++ Easy/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0) {
+            return "";
+        }
+        //Take first string as initial prefix
+        String prefix = strs[0];
+
+        //compare with each subsequent string
+        for(int i =0; i < strs.length; i++) {
+
+            //keep reducing prefix until it matches start of current string
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() -1);
+                    if(prefix.isEmpty()) {
+                        return "";
+                    }
+            }
+
+        }
+        return prefix;
+    }
+
+}
