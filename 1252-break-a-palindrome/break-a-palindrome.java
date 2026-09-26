5dc2a87b039184e29c62b05fa1f66fc0bc116d87
Sikhar Sethi <sikharsethi5@gmail.com>
2026-09-26 23:43:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (77.83%) - LeetSync

---
+++ 1252-break-a-palindrome/break-a-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+       int n = palindrome.length();
+
+       if (n <= 1) {
+        return "";
+       } 
+
+       char[] chars = palindrome.toCharArray();
+
+       for (int i = 0; i < n / 2; i++) {
+        if (chars[i] != 'a') {
+            chars[i] = 'a';
+            return new String (chars);
+        }
+       }
+
+       chars[n-1] = 'b';
+       return new String (chars);
+    }
+}
